b27c6d3d3929f997bc982ec69b85ba8c00107997
arandel1 <allison.randel@gmail.com>
2024-06-04 12:22:20 -0500

    removing resend functionality for now

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,23 +1,23 @@
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: fromEmail,
-      to: ['allison@allisonrandel.com'],
-      subject: 'Hello world',
-      react: <>Email me!</>,
-    });
+// export async function POST() {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: fromEmail,
+//       to: ['allison@allisonrandel.com'],
+//       subject: 'Hello world',
+//       react: <>Email me!</>,
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
